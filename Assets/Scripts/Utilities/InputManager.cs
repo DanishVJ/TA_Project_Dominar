08665c0f140319c0122886bc634ea7823e11373e
MYBVC\d.jacob629 <d.jacob629@mybvc.ca>
2026-07-29 14:50:44 -0600

    Code cleanup
    Added input manager, added singleton

---
+++ Assets/Scripts/Utilities/InputManager.cs
@@ -0,0 +1,55 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : Singleton<InputManager>
+{
+    private PlayerControls _controls;
+    private PlayerController _playerController;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _controls = new PlayerControls();
+        _controls.Enable();
+        _playerController = FindFirstObjectByType<PlayerController>();
+    }
+
+    private void OnEnable()
+    {
+        _controls.Player.Move.performed += OnMove;
+        _controls.Player.Move.canceled += OnMove;
+        
+        _controls.Player.Jump.performed += OnJump;
+    }
+    
+    private void OnDisable()
+    {
+        _controls.Player.Move.performed -= OnMove;
+        _controls.Player.Move.canceled -= OnMove;
+        
+        _controls.Player.Jump.performed -= OnJump;
+    }
+    
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _playerController.moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        _playerController.OnJump();
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
